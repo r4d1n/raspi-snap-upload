766d57c895948179f72321aade643bc0552f478b
Jared Radin <jared.radin@gmail.com>
2016-01-02 13:19:37 -0800

    take a picture and exit the process

--- index.babel.js
+++ index.babel.js
@@ -10,12 +10,28 @@ console.log('#### timestamp ####', timestamp)
 
 const camera = new RaspiCam({
   mode: 'photo',
-  output: path
+  output: path,
+  quality: 100,
+  width: 1000,
+  height: 700,
+  timeout: 2000,
+  encoding: 'jpg',
+  awb: 'auto'
 })
 
-
 camera.on("read", (err, timestamp, filename) => {
-    if (err) console.error(err)
-    console.log('took a picture')
-    console.log('callback timestamp and filename', timestamp, filename)
-});
+  if (err) console.error(err)
+  console.log('callback timestamp and filename', timestamp, filename)
+  console.log('took a picture')
+  process.exit(0)
+})
+
+camera.on("started", () => {
+  console.log('camera start', moment().format())
+})
+
+camera.on("exited", () => {
+  console.log('camera exit', moment().format())
+})
+
+camera.start()
