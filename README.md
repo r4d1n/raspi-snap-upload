ead2863323bd2c1deb46a0eb8f07b54171b886a9
Jared Radin <jared.radin@gmail.com>
2016-09-19 20:18:26 -0700

    update to use usb webcam with fswebcam and a child process

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # raspi-snap-upload
-Take a picture with a Raspberry Pi and upload it to S3
+Take a picture with a Raspberry Pi + USB webcam and upload it to S3
 
-Must make local versions of the
-aws.json file and tmp directory 
-as they are left out of the repo.
+Must have `fswebcam` installed (use apt-get) https://www.raspberrypi.org/documentation/usage/webcams/
+
+Must make local version of the config.json file and tmp directory. They are left out of the repo.
+
+example config:
+```
+{
+  "bucket": "your-bucket",
+  "height": "1080",
+  "width": "1920",
+  "tmpDir": "./tmp",
+  "s3Prefix": "images"
+}
+```
--- index.babel.js
+++ index.babel.js
@@ -1,65 +1,56 @@
 'use strict'
 
 const fs = require('fs')
-const RaspiCam = require('raspicam')
+const exec = require('child_process').exec
+
 const moment = require('moment')
 
 const AWS = require('aws-sdk')
 const s3 = new AWS.S3()
-const bucket = require('./aws.json').bucket; // config file
 
-const path = `./tmp/`
+const config = JSON.parse(fs.readFileSync('./config.json'))
+
+const bucket = config.bucket
+const height = config.height
+const width = config.width
+const tmpDir = config.tmpDir
+
 const timestamp = moment().format("YYYY-MM-DD@HH:mm")
 const name = `${timestamp}.jpg`
 
-console.log('#### name ####', name)
+console.log(`#### making a new exposure #### ${name}`)
 
-const camera = new RaspiCam({
-  mode: 'photo',
-  output: path + name,
-  quality: 100,
-  width: 1000,
-  height: 700,
-  encoding: 'jpg',
-  awb: 'auto',
-  timeout: 0
-})
-
-// camera event handler
-camera.on("read", (err, timestamp, filename) => {
-  if (err) console.error(err)
-
-  console.log(`read event filename: ${filename}`)
-
-  if (!filename.match(/~$/)) { // ignore non-photo file
-    putImage(path, filename)
-    .then((data) => {
-      console.log('Upload Succeeded')
-      console.log('AWS Response:', data)
-      return cleanCapturedImages(path, filename)
-    })
-    .then(() => {
-      process.exit(0)
-    })
-    .catch((err) => {
-      console.error(err)
-      process.exit(1)
-    })
+exec(`fswebcam -r ${config.width}x${config.height} --no-banner ${tmpDir}/${name}`, (err, stdout, stderr) => {
+  if (err) {
+    console.error(err)
+    return
   }
+
+  putImage(tmpDir, name)
+  .then((data) => {
+    console.log('Upload Succeeded')
+    console.log('AWS Response:', data)
+    return cleanCapturedImages(tmpDir)
+  })
+  .then(() => {
+    process.exit(0)
+  })
+  .catch((err) => {
+    console.warn(err)
+    process.exit(1)
+  })
 })
 
-camera.start() // trigger the camera
-
-function putImage(path, filename) {
+function putImage(directory, filename) {
   return new Promise((resolve,reject) => {
-    fs.stat(path + filename, (err, info) => { // to get file content length
+    fs.stat(`${directory}/${filename}`, (err, info) => { // to get file content length
       if (err) console.warn(err)
 
       let params = {
         Bucket: bucket,
-        Key: filename,
+        Key: `${config.s3Prefix}/${filename}`,
         ContentLength: info.size,
-        Body: fs.createReadStream(path + filename)
+        Body: fs.createReadStream(`${directory}/${filename}`)
       };
 
       s3.putObject(params, (err, data) => {
@@ -73,7 +64,7 @@ function putImage(path, filename) {
   })
 }
 
-function cleanCapturedImages(dir, filename) {
+function cleanCapturedImages(dir) {
   return new Promise((resolve, reject) => {
     fs.readdir(dir, (err, files) => {
       if (err) {
